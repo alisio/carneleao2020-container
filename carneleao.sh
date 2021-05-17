07ac597309aec51d1ab86ec8cc1cb4205003ba17
Antonio Alisio de Meneses Cordeiro <alisio.meneses@gmail.com>
2021-05-17 15:57:58 -0300

    Corrigir erro no script de inicialização do container

--- carneleao.sh
+++ carneleao.sh
@@ -4,5 +4,5 @@ socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 IP=`ifconfig ${dev_eth} | egrep inet | sed 's/.*inet \(.*\) netmask.*/\1/g'`
 docker run  --rm \
   -e DISPLAY=${IP}:0 \
-  -v ${HOME}/Documents/ProgramasRFB/LEAO2020-teste:/root/ProgramasRFB/LEAO2020 \
+  -v ${HOME}/Documents/ProgramasRFB/LEAO2020:/root/ProgramasRFB/LEAO2020 \
   alisio/carneleao2020
