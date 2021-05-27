a74447928a203b83ca4911a2a89d8512433a1371
Antonio Alisio de Meneses Cordeiro <alisio.meneses@gmail.com>
2021-05-27 06:40:52 -0300

    script de inicializacao nao executa quando ipv6 habilitado

--- carneleao.sh
+++ carneleao.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 dev_eth='en0'
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-IP=`ifconfig ${dev_eth} | egrep inet | sed 's/.*inet \(.*\) netmask.*/\1/g'`
+IP=`ifconfig ${dev_eth} |egrep -v inet6 | egrep inet | sed 's/.*inet \(.*\) netmask.*/\1/g'`
 docker run  --rm \
   -e DISPLAY=${IP}:0 \
   -v ${HOME}/Documents/ProgramasRFB/LEAO2020:/root/ProgramasRFB/LEAO2020 \
